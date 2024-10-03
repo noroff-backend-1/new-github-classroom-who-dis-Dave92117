a638de68580dac5d7436e280f182597956e5a113
David Instefjord <davidinstefjord@gmail.com>
2024-10-03 10:31:45 +0200

    made edits for button icon

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ function changeMode () {
  if (isDark === true) {
     $("body").css("background-color","white").css("color","#111")
     $("h1").css("color","#111")
-    $("button").innertext = "Dark";
+    $("button").text("🌑")
     
 } else {
      $("body").css("background-color","#111").css("color","aliceBlue")
      $("h1").css("color","aliceBlue")
-     $("button").innertext = "light";
+     $("button").text("🌕")
  }
     isDark =!isDark;
 }
